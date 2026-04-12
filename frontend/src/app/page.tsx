12328551969303457e9bbdb29be93930bf3d7be3
Артём <2023-00613@students.ssau.ru>
2026-04-13 00:35:57 +0400

    feat: base main page layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,25 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-    return <div className={styles.page}></div>;
+    return (
+        <div className={styles.layout}>
+            <div className={styles.container}>
+                <div className={styles.url_container}>
+                    <input type="text" placeholder="Paste Your URL..."></input>
+                </div>
+
+                <div className={styles.info_container}>
+                    <div className={styles.video_container}>
+                        <img src={"favicon.ico"} alt="Video Preview" />
+                        <h1>Дыо против Жотары. Кабачковое противостояние</h1>
+                    </div>
+                    <div className={styles.control_container}>
+                        <button className={styles.button_control}>Video</button>
+                        <button className={styles.button_control}>Audio</button>
+                        <button className={styles.button_download}>Download</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
