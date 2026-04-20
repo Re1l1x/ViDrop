e9e2140bb1d47128f57974c81f9b58771ae032cd
Артём <artem.viryaskin@gmail.com>
2026-04-20 18:01:29 +0400

    feat: add animation and layout changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Montserrat, Audiowide } from "next/font/google";
 import "./globals.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
+const audiowide = Audiowide({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "ViDrop",
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     const [inputUrl, setInputUrl] = useState<string>("");
     const [downloadUrl, setDownloadUrl] = useState<string>("");
 
+    const [isUrlEntered, setIsUrlEntered] = useState<boolean>(false);
+    const targetLength = 43;
+
     async function Download() {
         try {
             const response = await fetch("http://localhost:8080/download", {
@@ -35,28 +38,55 @@ export default function Home() {
         const input = e.target.value;
 
         setInputUrl(input);
+        if (input.length >= targetLength) {
+            setIsUrlEntered(true);
+        }
     };
 
     return (
         <div className={styles.layout}>
             <div className={styles.container}>
-                <div className={styles.url_container}>
+                <div className={styles.input_line_container}>
+                    <div className={styles.title}>ViDrop</div>
                     <input onChange={onChangeUrl} type="text" placeholder="Paste Your URL..."></input>
                 </div>
 
-                <div className={styles.info_container}>
+                <div className={`${styles.info_container} ${isUrlEntered ? styles.expanded : styles.collapsed}`}>
+                    {/* <div className={`${styles.form_box} ${isSignIn ? "" : styles.active}`}></div> */}
                     <div className={styles.video_container}>
+                        {/* <div className={styles.section_name}>Video</div> */}
                         <img src={"favicon.ico"} alt="Video Preview" />
-                        <h1>Дыо против Жотары. Кабачковое противостояние</h1>
+                        <div className={styles.video_name}>Дыо против Жотары. Кабачковое противостояние</div>
                     </div>
                     <div className={styles.control_container}>
-                        <button className={styles.button_control} onClick={Download}>
-                            Video
-                        </button>
-                        <button className={styles.button_control}>Audio</button>
-                        <button className={styles.button_download} onClick={getVideo}>
-                            Download
-                        </button>
+                        <div className={styles.control_row}>
+                            <div className={styles.section_name}>Settings:</div>
+                        </div>
+                        <div className={styles.control_row}>
+                            <button className={styles.button_control} onClick={Download}>
+                                Video
+                            </button>
+                            <button className={styles.button_control}>Audio</button>
+                        </div>
+                        <div className={styles.control_row}>
+                            <div className={styles.section_name}>Quality:</div>
+                            <select className={styles.quality_selector} name="Quality">
+                                <option value="" disabled selected>
+                                    Select Quality
+                                </option>
+                                <option value="">144p</option>
+                                <option value="">240p</option>
+                                <option value="">360p</option>
+                                <option value="">480p</option>
+                                <option value="">720p</option>
+                                <option value="">1080p</option>
+                            </select>
+                        </div>
+                        <div className={styles.control_row}>
+                            <button className={styles.button_download} onClick={getVideo}>
+                                Download
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
